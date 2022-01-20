9549c11d7ffcbcfb5e507ab3410b99185f2afa1d
Yeseul Lee <sdc01194@gmail.com>
2022-01-20 16:47:40 +0900

    [common_widget] add svg icon

---
+++ packages/common_widgets/lib/icons/icon_16.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AppIcon16 extends StatelessWidget {
+  final String resource;
+  final BoxFit? fit;
+  final Color? color;
+
+  const AppIcon16(
+    this.resource, {
+    Key? key,
+    this.fit,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 16,
+      height: 16,
+      child: SvgPicture.asset(
+        resource,
+        fit: fit ?? BoxFit.contain,
+        color: color,
+      ),
+    );
+  }
+}
