0979f02f50ea6e95ee7c715259a0d2aee60a23e9
Yeseul Lee <sdc01194@gmail.com>
2022-01-21 15:35:55 +0900

    [common_widget] add text view

---
+++ packages/common_widgets/lib/texts/button1.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TextButton1 extends StatelessWidget {
+  const TextButton1(
+    this.data, {
+    Key? key,
+    this.color,
+    this.height,
+    this.wordSpacing,
+    this.overflow,
+    this.decoration,
+    this.maxLines,
+    this.softWrap,
+    this.textAlign,
+    this.textHeightBehavior,
+  }) : super(key: key);
+
+  final String data;
+
+  final Color? color;
+  final double? height;
+  final double? wordSpacing;
+  final TextOverflow? overflow;
+  final TextDecoration? decoration;
+
+  final int? maxLines;
+  final bool? softWrap;
+  final TextAlign? textAlign;
+  final TextHeightBehavior? textHeightBehavior;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data,
+      style: Theme.of(context).textTheme.button?.copyWith(
+            color: color,
+            height: height,
+            wordSpacing: wordSpacing,
+            decoration: decoration,
+          ),
+      maxLines: maxLines,
+      softWrap: softWrap,
+      textAlign: textAlign,
+      overflow: overflow,
+      textHeightBehavior: textHeightBehavior ??
+          const TextHeightBehavior(
+            applyHeightToFirstAscent: false,
+            applyHeightToLastDescent: false,
+          ),
+    );
+  }
+}
